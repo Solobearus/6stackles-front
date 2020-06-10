112c9fba01e0d04f204dab8f73afaa2a6e05e319
natalie <natalie@enkin.co.il>
2020-06-10 15:57:51 +0300

    adding signup inputs

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import './SignUp.css'
+import React, { useSelector } from "react";
+import "./SignUp.css";
+import Button from "../_shared/Button/Button";
 
+const SignUp = () => {
+  return (
+    <div className="signUp" data-testid="signUp">
+      <div className="signUp_welcome">Register here</div>
+      <input
+        type="text"
+        className="signUp_input"
+        placeholder={`test@test.co.il`}
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        className="signUp_input"
+        placeholder={`123456`}
+        onChange={(e) => {
+          console.log(e.target.value);
+        }}
+      />
+      <Button
+        onClick={(e) => {
+          console.log("signed up");
+        }}
+      />
+    </div>
+  );
+};
 
-const SignUp= () => {
-    return (
-        < div className="signUp" data-testid="signUp">
-
-        </div >
-    )
-}
-
-export default SignUp
+export default SignUp;
