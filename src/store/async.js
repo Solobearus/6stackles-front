cf1b6cafa4352042f0ef46dcd44f9a14d5d9499d
natalie <natalie@enkin.co.il>
2020-06-11 14:37:08 +0300

    in store adding register async function

--- src/store/async.js
+++ src/store/async.js
@@ -1,19 +1,42 @@
+const login = ({ email, password }) =>
+  fetch(
+    `http://localhost:${process.env.REACT_APP_PORT_OF_GATEWAY}/auth/signin`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        // "email": email,
+        // "password": password
+        email: "321@ab.cd",
+        password: "a1",
+      }),
+      headers: { "Content-Type": "application/json" },
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res)
+    .catch((err) => console.error(err));
 
-export const login = ({ email, password }) => (
+const register = ({ email, password, phone }) =>
+  fetch(
+    `http://localhost:${process.env.REACT_APP_PORT_OF_GATEWAY}/auth/signup`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        // "email": email,
+        // "password": password,
+        //"phone":phone
+        email: "321@ab.cd",
+        password: "a1",
+        phone: 1234567,
+      }),
+      headers: { "Content-Type": "application/json" },
+    }
+  )
+    .then((res) => res.json())
+    .then((res) => res)
+    .catch((err) => console.error(err));
 
-    fetch(`http://localhost:${process.env.REACT_APP_PORT_OF_GATEWAY}/auth/signin`,
-        {
-            method: 'POST',
-            body: JSON.stringify({
-                // "email": email,
-                // "password": password
-                email: "321@ab.cd",
-                password: "a1"
-            }),
-            headers: { 'Content-Type': 'application/json' }
-        }
-    )
-        .then(res => res.json())
-        .then(res => res)
-        .catch(err => console.error(err))
-)
+module.exports = {
+  login,
+  register,
+};
