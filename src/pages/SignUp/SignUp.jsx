0cc947ba8372a30fb3e23ea3444c3b58870ee2ed
natalie <natalie@enkin.co.il>
2020-06-11 14:28:27 +0300

    signup page:adding more inputs

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -3,14 +3,20 @@ import "./SignUp.css";
 import Button from "../../components/Button/Button";
 import { useSelector, useDispatch } from "react-redux";
 import Input from "../../components/Input/Input";
-// import { register } from "../../store/async";
+import { register } from "../../store/async";
 
 const SignUp = () => {
   const { text } = useSelector((state) => state.language);
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const checkPassword = () => {};
 
+  const handleSubmit = async () => {
+    const result = await register(email, password, phone);
+    dispatch(userAuthSlice.actions.setToken(result.token));
+  };
   return (
     <div className="signUp" data-testid="signUp">
       <div className="signUp_welcome">{text.sign_up.welcome}</div>
@@ -27,13 +33,26 @@ const SignUp = () => {
           setPassword(e.target.value);
         }}
       />
+      <Input
+        type="password"
+        placeholder={text.general.repeat_password}
+        onChange={(e) => {
+          checkPassword(e.target.value);
+        }}
+      />
+      <Input
+        type="number"
+        placeholder={text.general.phone}
+        onChange={(e) => {
+          setPhone(e.target.value);
+        }}
+      />
 
       <Button
         value={text.sign_up.submit}
-        onClick={async () => {
-          // const result=await register(param,param,param)
-          // dispatch(userAuthSlice.actions.setToken(result.token))
+        onClick={() => {
           console.log("signed up");
+          handleSubmit();
         }}
       />
     </div>
