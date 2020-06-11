4c39d4f8352637f6a933ee92b99c783cd2d15b73
natalie <natalie@enkin.co.il>
2020-06-11 16:10:33 +0300

    products page: adding input

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import "./Products.css";
 import Button from "../../components/Button/Button";
 import { useSelector, useDispatch } from "react-redux";
-import { login } from "../../store/async";
-import { userAuthSlice, userDetailsSlice } from "../../store/slices";
 import Input from "../../components/Input/Input";
 
 const Products = () => {
-  return <div className="products" data-testid="products"></div>;
+  const { text } = useSelector((state) => state.language);
+
+  const search = () => {
+    console.log("dispatch a search in products...");
+  };
+  return (
+    <div className="products" data-testid="products">
+      <Input
+        type="text"
+        placeholder={text.general.search}
+        value={text}
+        onChange={(e) => search(e.target.value)}
+      />
+      <div>products..</div>
+    </div>
+  );
 };
 
 export default Products;
