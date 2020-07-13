027153d5ac88f86dce0516f6fea73b638eb868c0
solobearus <ivansolobear@gmail.com>
2020-07-13 19:22:09 +0300

    added placeholder image for when image is null in products

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./ProductItem.css";
 import ProductInfo from "../ProductInfo/ProductInfo";
-const ProductItem = (props) => {
+import imagePlaceholder from "../../images/imagePlaceholder.jpg";
+
+const ProductItem = ({ name, desc, id, price, imgUrl }) => {
   return (
     <li className="product_item">
-      <ProductInfo name={props.name} desc={props.desc} id={props.id} />
+      <ProductInfo name={name} desc={desc} id={id} />
       <div className="product_item_img_wrapper">
-        <img src={props.imgUrl} alt={props.name} className="product_item_img" />
+        <img
+          src={imgUrl ? imgUrl : imagePlaceholder}
+          alt={name}
+          className="product_item_img"
+        />
         <div className="product_item_img_footer">
-          <span>{props.price} $</span>
+          <span>{price} $</span>
           <span> 12 km</span>
         </div>
       </div>
