ef5fac09715e9a08acc6517e06de25d2762e9fbe
natalie <natalie@enkin.co.il>
2020-07-19 14:55:27 +0300

    sidemenue styling: adding css transition

--- src/components/SideMenue/SideMenue.jsx
+++ src/components/SideMenue/SideMenue.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import MenueLink from "../MenueLink/MenueLink";
-import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./SideMenue.css";
 
 const SideMenue = ({ open, setOpen }) => {
